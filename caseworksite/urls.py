b9054ca1722c17404d5b9ffe79b7cd912ea5e056
James Wilson <oh.that.wilson@googlemail.com>
2017-03-03 14:18:34 +0000

    Added a link to the admin page to base.html, and the required bits in urls.py for the site to make it work.

--- caseworksite/urls.py
+++ caseworksite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('caseworkclub.urls')),
+    url(r'^', include('caseworkclub.urls'),name='admin'),
 ]
